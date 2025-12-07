bfe109ed891038413845006794130b0e0a6178fc
yingyu5658 <i@yingyu5658.me>
2025-12-07 12:17:37 +0800

    solve 1

---
+++ 0001/main.c
@@ -0,0 +1,26 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ * 这是一个暴力解法
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
+    // 可访问的最大索引
+    int nums_max_idx = numsSize - 1;
+
+    for (int i = 0; i < nums_max_idx; i++) {
+        for (int j = i + 1; j <= nums_max_idx; j++) {
+            if (nums[i] + nums[j] == target) {
+                int *rtn = malloc(sizeof(int) * 2);
+                rtn[0] = i;
+                rtn[1] = j;
+                *returnSize = 2;
+                return rtn;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    return NULL;
+}
